692aff0d571c6e2156eaa91a200ed2c059800d59
sabrek15 <reddy.ksb007@gmail.com>
2023-02-18 16:15:46 +0530

    js

--- js/external.js
+++ js/external.js
@@ -3,11 +3,3 @@ function getComputerChoice() {
     const randoption = options[Math.floor(Math.random()*options.length)];
     return randoption;
 }
-
-const buttons = document.querySelectorAll('button');
-  buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        let playerchoice = event.target.id;
-        console.log(playerchoice);
-    });
-  });
