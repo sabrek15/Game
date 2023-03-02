a24cd2b92e6cdcbd5bc2eaa994ba9de0a69095b0
sabrek15 <reddy.ksb007@gmail.com>
2023-03-02 22:57:28 +0530

    js/external.js

--- js/external.js
+++ js/external.js
@@ -1,3 +1,5 @@
+const buttons = document.querySelectorAll('.btn');
+
 function getComputerChoice() {
     const options = ["rock","papper","scissor"];
     const randoption = options[Math.floor(Math.random()*options.length)];
