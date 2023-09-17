acc6ccf549f1cdcf846b61413b1441afcfecfc71
sabrek15 <reddy.ksb007@gmail.com>
2023-09-17 23:32:27 +0530

    changed the fuctions of game

--- js/external.js
+++ js/external.js
@@ -1,7 +1,57 @@
-const buttons = document.querySelectorAll('.btn');
+const buttons = document.querySelectorAll("button");
+const resultParagraph = document.getElementById("result");
+let userScore = 0;
+let computerScore = 0;
+let turns = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (turns < 5) {
+            const userChoice = button.id;
+            const computerChoice = getComputerChoice();
+            const result = determineWinner(userChoice, computerChoice);
+            displayResult(result, computerChoice);
+            updateScores(result);
+            turns++;
+            if (turns === 5) {
+                displayFinalResult();
+            }
+        }
+    });
+});
 
 function getComputerChoice() {
-    const options = ["rock","papper","scissor"];
-    const randoption = options[Math.floor(Math.random()*options.length)];
-    return randoption;
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return "You win!";
+    } else {
+        return "Computer wins!";
+    }
+}
+
+function displayResult(result, computerChoice) {
+    resultParagraph.innerHTML = `Computer chose ${computerChoice}. ${result}`;
+}
+
+function updateScores(result) {
+    if (result === "You win!") {
+        userScore++;
+    } else if (result === "Computer wins!") {
+        computerScore++;
+    }
+}
+
+function displayFinalResult() {
+    resultParagraph.innerHTML = `Game Over! Your Score: ${userScore}, Computer Score: ${computerScore}`;
 }
